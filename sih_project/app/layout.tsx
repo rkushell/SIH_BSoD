552781bc795a881dee088b0c735402807c890abd
Srinidhi <srinidhi@Srinidhis-MacBook-Air.local>
2025-12-02 13:40:45 +0530

    Favicon changes - Not useful

--- sih_project/app/layout.tsx
+++ sih_project/app/layout.tsx
@@ -15,20 +15,20 @@ export const metadata: Metadata = {
   generator: "v0.app",
   icons: {
     icon: [
+      // {
+      //   url: "/icon-light-32x32.png",
+      //   media: "(prefers-color-scheme: light)",
+      // },
+      // {
+      //   url: "/icon-dark-32x32.png",
+      //   media: "(prefers-color-scheme: dark)",
+      // },
       {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
+        url: "/favicon.png",
+        //type: "image/svg+xml",
       },
     ],
-    apple: "/apple-icon.png",
+    //apple: "/apple-icon.png",
   },
 }
 
